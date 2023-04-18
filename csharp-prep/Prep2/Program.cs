0a3cae4ccefe0edc7d07b602927333846f11fd67
polive92 <pesiolive@gmail.com>
2023-04-18 15:41:08 -0600

    changed code to calculate grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string grade = "";
+
+        if (percent >= 90)
+        {
+            grade = "A";
+
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if (percent >= 70)
+
+        {
+            grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your current grade is: {grade}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("YOU PASSED!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass. Maybe next time!");
+        }
     }
 }
