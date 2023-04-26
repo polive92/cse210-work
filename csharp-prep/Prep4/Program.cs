3866d14a087687d534122497161bf8b79d792d98
polive92 <pesiolive@gmail.com>
2023-04-26 16:13:22 -0600

    wrote code for an appending list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+       Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+       List<int> numbers = new List<int>();
+       while (true) {
+        
+       
+       Console.Write("Enter number: ");
+       int num = int.Parse(Console.ReadLine());
+       if (num == 0) {
+           break;
+       }
+       numbers.Add(num);
+
+       }
+       Console.WriteLine("The sum is: {0}", numbers.Sum());
+       Console.WriteLine("The average is: {0}", numbers.Average());
+       Console.WriteLine("The largest number is: {0}", numbers.Max());
+      
     }
 }
