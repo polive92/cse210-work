d344d9d8800563b7a535ab91ea8ff56488e9bb28
polive92 <pesiolive@gmail.com>
2023-04-26 13:41:53 -0600

    wrote code for game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       Random randomGenerator = new Random();
+       int magicNumber = randomGenerator.Next(1, 100);
+
+       int userGuess = 0;
+
+
+       do {
+    
+        Console.Write("What is your guess? ");
+        userGuess = int.Parse(Console.ReadLine());
+
+        if (userGuess < magicNumber) {
+            Console.WriteLine("Higher");
+        } else if (userGuess > magicNumber) {
+            Console.WriteLine("Lower");
+        } else {
+            Console.WriteLine("You guessed it!");
+        }
+       } while (userGuess != magicNumber);
+
+
     }
+  
 }
