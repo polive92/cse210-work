b82641d1d8a655fd383b4c9eedbf184cd78589f6
polive92 <pesiolive@gmail.com>
2023-05-02 18:48:51 -0600

    put job information is different variables

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._myTitle = "Software Engineer";
+        job1._companyTitle = "Microsoft";
+        job1._beginningYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._myTitle = "Manager";
+        job2._companyTitle = "Apple";
+        job2._beginningYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._myName = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+     
+
     }
 }
